226f33e3a55e39483052eb8b845f63d24e4c2bf7
Kenny Lee <kenjlee09@gmail.com>
2025-07-13 20:50:11 -0700

    enemy autism (AI)

--- Project X/Assets/Scripts/Combat/EnemyMovement.cs
+++ Project X/Assets/Scripts/Combat/EnemyMovement.cs
@@ -1,16 +1,45 @@
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public TriggerDetector enemyVision;
+    public PlayerMovement player;
+    public Rigidbody enemyRb;
+    public float enemyMoveSpd;
+    public float enemyAttackStr;
+    public bool enemyAggroed;
+
+    public void Start()
     {
-        
+        enemyRb = GetComponent<Rigidbody>();
+        enemyRb.constraints = RigidbodyConstraints.FreezeRotation;
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+        enemyAggroed = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        IsEnemyAggroed();
+        EnemyChasePlayer();
     }
+
+    public void IsEnemyAggroed()
+    {
+        if (enemyVision.triggerEntered && !enemyAggroed)
+        {
+            Debug.Log("Enemy Detected Player");
+            enemyAggroed = true;
+        }
+    }
+
+    public void EnemyChasePlayer()
+    {
+        if (enemyAggroed)
+        {
+            Vector3 moveDir = player.playerBody.position - transform.position;
+            enemyRb.AddForce(moveDir.normalized * enemyMoveSpd);
+        }
+    }   
 }
--- Project X/Assets/Scripts/TriggerDetector.cs
+++ Project X/Assets/Scripts/TriggerDetector.cs
@@ -4,10 +4,20 @@ public class TriggerDetector : MonoBehaviour
 {
     //script to tell parent object when a child trigger has been entered b/c Unity :D
     public bool triggerEntered;
-
-    void OnTriggerStay()
+    [Tooltip("Set to true if trigger only meant to detect player")]
+    public bool playerDetector;
+    void OnTriggerStay(Collider other)
     {
-        triggerEntered = true;
+        if (playerDetector)
+        {
+            if (other.CompareTag("Player"))
+            {
+                triggerEntered = true;
+            }
+        } else
+        {
+            triggerEntered = true;
+        }
     }
 
     void OnTriggerExit()
