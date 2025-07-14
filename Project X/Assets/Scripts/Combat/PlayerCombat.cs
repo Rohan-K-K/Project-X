e156aa04ed583bfb37d5dee64fc45c9169326ded
Kenny Lee <kenjlee09@gmail.com>
2025-07-13 22:36:10 -0700

    enemystuff

---
+++ Project X/Assets/Scripts/Combat/PlayerCombat.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public GameObject swingAnimation;
+    public PlayerInputs inputs;
+
+    public Animator animator;
+
+    public void Start()
+    {
+        inputs = GetComponent<PlayerInputs>();
+    }
+
+    public void Update()
+    {
+    }
+
+    public void basicAttack()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Instantiate(swingAnimation, transform.position, transform.rotation);
+            animator.Play(swingAnimation, -1, 0f);
+        }
+    }
+}
