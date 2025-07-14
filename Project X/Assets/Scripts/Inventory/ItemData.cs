4d792fcf753bf1a7e5b3d95e341193c5406defdb
Rohan-K-K <rkonchig@uci.edu>
2025-07-14 11:29:02 -0700

    starting inventoy scripts and styff
    i can spel

---
+++ Project X/Assets/Scripts/Inventory/ItemData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemData", menuName = "Scriptable Objects/ItemData")]
+public class ItemData : ScriptableObject
+{
+    public string itemName;
+    public Sprite sprite;
+    public float attackStr;
+    
+}
